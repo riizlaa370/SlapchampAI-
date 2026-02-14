7bb183d7932bd0816f366eed95660be33b14b877
riizlaa370 <riizlaa370@gmail.com>
2026-02-14 19:06:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -130,7 +130,7 @@ def get_slap_gif():
 # ── Polling ─────────────────────────────────────────────────────────────────
 bot_username = "slapchampai"
 MAX_REPLIES_PER_POLL = 3
-POLL_INTERVAL_SEC = 9999999
+POLL_INTERVAL_SEC = 999999999999999
 
 while True:
     try:
